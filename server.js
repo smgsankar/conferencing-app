5c48704951926cf18d66b652b0adc5f40b22c1a1
smgsankar <gomathi.sankar@shopup.org>
2024-06-07 02:54:18 +0530

    fix: allow all origins in cors for socket to allow FE app to connect

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
 import express from "express";
-import { createServer } from "http";
-import path from "path";
 import { Server } from "socket.io";
+import { createServer } from "http";
 
 const app = express();
+
 const server = createServer(app);
-const io = new Server(server);
-
-app.use(express.static(path.join(path.resolve(), "/public")));
-
-app.get("/", (_, res) => {
-  res.sendFile(path.join(path.resolve(), "/index.html"));
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
 });
 
 let participants = [];
