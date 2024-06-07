9dc0da71d85b6689633b301ed53ce1813c36bb95
smgsankar <gomathi.sankar@shopup.org>
2024-06-07 15:10:22 +0530

    fix: show user presentation on screen share

--- app/src/meetroom/components/UserTiles.tsx
+++ app/src/meetroom/components/UserTiles.tsx
@@ -1,3 +1,21 @@
+import { useEffect, useRef } from "react";
+import { useMeetRoomState } from "../../hooks/useMeetRoomState";
+
 export const UserTiles = () => {
-  return <section id="presentation"></section>;
+  const ref = useRef<HTMLVideoElement>(null);
+  const { displayStream } = useMeetRoomState();
+
+  useEffect(() => {
+    if (ref.current && displayStream) {
+      ref.current.srcObject = displayStream;
+    }
+  }, [displayStream]);
+
+  return (
+    <section id="presentation">
+      {!!displayStream && (
+        <video muted autoPlay ref={ref} style={{ width: "100%" }} />
+      )}
+    </section>
+  );
 };
